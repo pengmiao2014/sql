a8afe4d626f23c23f11b0ce6fa6f6d4c90fa033a
pengmiao2014 <pengmiao2014@gmail.com>
2018-09-19 00:07:12 -0700

    Update leetcode.sql

--- leetcode.sql
+++ leetcode.sql
@@ -36,7 +36,19 @@ WHERE
             Employee
         GROUP BY DepartmentId
     )
-;        
+
+-- 177. Nth Highest Salary  ---if not exsit will return null!!
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M=N-1;
+  RETURN (   
+      select distinct Salary 
+      from Employee
+      order by Salary desc
+      limit 1 offset m       
+  );
+END
         
 select name
 from customer
